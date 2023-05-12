e66e18ba2305ed02cb89759e78023d10e71d4950
Valery Gonzalez <valgonzr@gmail.com>
2023-05-12 16:03:17 -0400

    comments working

---
+++ models/comment.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  post: {
+    type: String,
+   ref: "Post"
+  },
+  comment:{
+    type: String,
+    required: true
+  },
+  
+  user: {
+    type: String,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("comment", CommentSchema);
